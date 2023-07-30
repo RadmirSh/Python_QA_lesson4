3afbfee35d15a9a527fa5d0c2071fa1fdcfba60c
Radmir Shagiev <veselday@gmail.com>
2023-07-30 18:22:57 +0500

    функции вывода элементов множества, также сортировки элементов

--- Task22.py
+++ Task22.py
@@ -2,3 +2,13 @@
 # которые встречаются в обоих наборах. Пользователь вводит 2 числа. n — кол-во элементов первого множества. m — кол-во элементов второго множества. 
 # Затем пользователь вводит сами элементы множеств.
 
+from random import randint     # необходима данная библиотека
+
+n_multiplicity = set(randint(1, 20) for i in range(int(input("Пожалуйста, введите кол-во элементов первого множества: "))))
+print(n_multiplicity)
+
+m_multiplicity = set(randint(1, 20) for i in range(int(input("Пожалуйста, введите кол-во элементов второго множества: "))))
+print(m_multiplicity)
+
+sort_multiplicity = sorted(n_multiplicity.intersection(m_multiplicity))      # сортировка элементов множества
+print(*sort_multiplicity)
